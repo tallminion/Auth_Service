defa2e3704066cbc68ab1252eba6d3c0022302a9
tallminion <sagarkr1109@gmail.com>
2024-09-22 00:07:12 +0530

    prepared User-repository

---
+++ src/repository/user-repository.js
@@ -0,0 +1,27 @@
+const { User } = require('../models/index');
+
+class UserRepository {
+    async create(data){
+        try {
+            const user = await User.create(data);
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository level");
+            throw error;
+        }
+    }
+
+    async destroy(userId){
+        try {
+            await user.destroy({
+                where:{
+                    id: userId
+                }
+            });
+            return true;
+        } catch (error) {
+            console.log("Something went wrong on repository level");
+            throw error;
+        }
+    }
+}
