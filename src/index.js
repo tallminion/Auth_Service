cd684f6446778174f686d134e1ed2303c85ae910
tallminion <sagarkr1109@gmail.com>
2024-09-21 22:59:40 +0530

    First commit: Project skeleton setup done

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { PORT } = require('./config/serverConfig');
+const app = express();
+
+const prepareAndStartServer = () => {
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+    } );
+}
+
+prepareAndStartServer();
