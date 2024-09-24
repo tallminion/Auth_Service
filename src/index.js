cc6f1103ca94dbcc4ed7c779dd7f2ed3ca46e430
tallminion <sagarkr1109@gmail.com>
2024-09-24 23:39:09 +0530

    added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const prepareAndStartServer = () => {
     app.use('/api', apiRoutes);
     app.listen(PORT, async () => {
         console.log(`Server started on port ${PORT}`);
-        const repo = new UserRepository();
+        // const repo = new UserRepository();
         // const response = await repo.getById(1);
         // console.log(response);
     } );
